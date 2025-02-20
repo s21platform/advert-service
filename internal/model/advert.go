011a0692104d26f41411eb50eb0b9fa06b1e66e1
Tony <akatkov89@mail.ru>
2025-02-20 20:01:54 +0300

    fix: marshaling of UserFilter struct moved to sqlx by redefining the Value and Scan methods

--- internal/model/advert.go
+++ internal/model/advert.go
@@ -1,33 +1,51 @@
 package model
 
 import (
+	"database/sql/driver"
 	"encoding/json"
+	"errors"
 	"time"
 
 	advert "github.com/s21platform/advert-proto/advert-proto"
 )
 
 type Advert struct {
-	OwnerUUID   string    `db:"owner_uuid"`
-	TextContent string    `db:"text_content"`
-	UserFilter  []byte    `db:"filter"`
-	ExpiresAt   time.Time `db:"expired_at"`
+	OwnerUUID   string     `db:"owner_uuid"`
+	TextContent string     `db:"text_content"`
+	UserFilter  UserFilter `db:"filter"`
+	ExpiresAt   time.Time  `db:"expired_at"`
 }
 
 type UserFilter struct {
 	Os string `json:"os"`
 }
 
-func (a *Advert) ToDTO(UUID string, in *advert.CreateAdvertIn) (Advert, error) {
-	userFilterJSON, err := json.Marshal(in.User)
+func (uf UserFilter) Value() (driver.Value, error) {
+	j, err := json.Marshal(uf)
 	if err != nil {
-		userFilterJSON = nil
+		return "", err
+	}
+	return string(j), nil
+}
+
+func (uf UserFilter) Scan(value interface{}) error {
+	b, isBytes := value.([]byte)
+	if !isBytes {
+		s, isString := value.(string)
+		if !isString {
+			return errors.New("failed to Scan lot.data field, supported types: `string` or `[]byte`")
+		}
+		b = []byte(s)
 	}
 
+	return json.Unmarshal(b, &uf)
+}
+
+func (a *Advert) ToDTO(UUID string, in *advert.CreateAdvertIn) (Advert, error) {
 	result := Advert{
 		OwnerUUID:   UUID,
 		TextContent: in.Text,
-		UserFilter:  []byte(userFilterJSON),
+		UserFilter:  UserFilter{Os: in.User.Os},
 		ExpiresAt:   in.ExpiredAt.AsTime(),
 	}
 
